6dc68ca53e1c110ede5178f1211557afbde4b57f
Artūras Šeršniovas <arturaz@gmail.com>
2019-11-08 11:26:06 +0200

    #1 Return back Repositories @method definitions

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -6,6 +6,12 @@ use App\Entity\Event;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
+/**
+ * @method Event|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Event|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Event[]    findAll()
+ * @method Event[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
 class EventRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
