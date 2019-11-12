972559494688885f4058e75a8c8c0c878362533b
Artūras Šeršniovas <arturaz@gmail.com>
2019-11-12 21:45:50 +0200

    Add DocBlock on VehicleType Form Type

--- src/Form/Type/VehicleType.php
+++ src/Form/Type/VehicleType.php
@@ -14,6 +14,10 @@ use Symfony\Component\Validator\Constraints\Length;
 
 class VehicleType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -48,6 +52,9 @@ class VehicleType extends AbstractType
         ;
     }
 
+    /**
+     * @param OptionsResolver $resolver
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
