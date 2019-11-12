9483e6f09f7db1d691fe65913303a4d1904a0714
Artūras Šeršniovas <arturaz@gmail.com>
2019-11-12 22:19:10 +0200

    Add UniqueEntity to Vehicle Entity

--- src/Entity/Vehicle.php
+++ src/Entity/Vehicle.php
@@ -7,10 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\DateTimeType;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\VehicleRepository")
+ * @UniqueEntity("vinCode")
+ * @UniqueEntity("registrationPlateNumber")
  */
 class Vehicle
 {
