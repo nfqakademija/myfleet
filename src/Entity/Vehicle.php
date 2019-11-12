a6ebbf7c5c8f418adec0e3b93f6f750e222bc31b
Artūras Šeršniovas <arturaz@gmail.com>
2019-11-12 22:20:38 +0200

    Remove unused use statement

--- src/Entity/Vehicle.php
+++ src/Entity/Vehicle.php
@@ -5,7 +5,6 @@ namespace App\Entity;
 use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\DateTimeType;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
