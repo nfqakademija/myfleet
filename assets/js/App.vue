54055aed65ebddf72019c4555386002c444e747f
jonagade <jonas.gadeikis9@gmail.com>
2019-11-17 21:17:48 +0200

    #1 Remove Unnecesarry Code

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -9,17 +9,11 @@
 </template>
 
 <script>
-	import Table from './components/Pages/List/Table';
-	import TheHeader from './components/TheHeader';
 	import BaseButton from './components/BaseComponents/BaseButton';
-	import Vehicle from './components/Pages/Vehicle/Vehicle';
 
 	export default {
 		components: {
-		    Table,
-			TheHeader,
 			BaseButton,
-			Vehicle,
 		},
 	}
 </script>
