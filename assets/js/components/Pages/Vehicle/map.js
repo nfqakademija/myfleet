a93259dec33688d999a97db2eff09d7870d274fe
jonagade <jonas.gadeikis9@gmail.com>
2019-11-22 17:58:57 +0200

    #2 Add Marker on Map

--- assets/js/components/Pages/Vehicle/map.js
+++ assets/js/components/Pages/Vehicle/map.js
@@ -1,5 +1,7 @@
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const Map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=0FpdZwCBDclducjXH2WE', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-}).addTo(mymap);
+}).addTo(Map);
+
+L.marker([51.505, -0.09]).addTo(Map);
