787047844469f1cefe35c309ef35cdeee693b964
jonagade <jonas.gadeikis9@gmail.com>
2019-11-22 17:18:51 +0200

    #2 Show Central London Coordinates on Map

--- assets/js/components/Pages/Vehicle/map.js
+++ assets/js/components/Pages/Vehicle/map.js
@@ -1,5 +1,5 @@
-import L from 'leaflet';
+const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-export default mymap;
+L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=0FpdZwCBDclducjXH2WE', {
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(mymap);
