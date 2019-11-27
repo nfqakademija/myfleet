061f13d5a61e90afccd9a4f39346d1fa268b2f02
Artūras Šeršniovas <arturaz@gmail.com>
2019-11-27 13:44:49 +0200

    Add endpoint URL based on ENV for RegistryDataImportService and VehicleDataImportService

--- src/Service/VehicleDataImportService.php
+++ src/Service/VehicleDataImportService.php
@@ -3,12 +3,16 @@
 namespace App\Service;
 
 use App\Entity\Vehicle;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class VehicleDataImportService
 {
-    private $url = 'http://127.0.0.1:8000/demo/api/vehicle_data/';
+    private $url;
+
+    protected $requestStack;
 
     private $httpClient;
 
@@ -16,10 +20,22 @@ class VehicleDataImportService
 
     private $vehicles;
 
-    public function __construct(HttpClientInterface $httpClient, ObjectManager $manager)
-    {
+    public function __construct(
+        KernelInterface $kernel,
+        RequestStack $requestStack,
+        HttpClientInterface $httpClient,
+        ObjectManager $manager
+    ) {
+        $this->requestStack = $requestStack;
+        $request = $this->requestStack->getCurrentRequest();
+        if ($kernel->getEnvironment() == 'prod') {
+            $this->url = $request->getSchemeAndHttpHost().':'.$request->getPort().'/demo/api/registry_data/';
+        } else {
+            $this->url = 'http://host.docker.internal:8000/demo/api/registry_data/';
+        }
+
         $this->httpClient = $httpClient;
-        $this->entityManager = $manager;//$this->getDoctrine()->getManager();
+        $this->entityManager = $manager;
         $this->vehicles = $manager->getRepository(Vehicle::class)->findAll();
     }
 
