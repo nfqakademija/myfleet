a57e2bc5141e9839ad4004975e2038e99d340c30
jonagade <jonas.gadeikis9@gmail.com>
2019-12-04 22:54:22 +0200

    #3 Change Filter Button Icon, Add Polyline To Map, Add Other Changes

--- assets/js/components/Pages/Vehicle/map.js
+++ assets/js/components/Pages/Vehicle/map.js
@@ -1,7 +1,27 @@
-const Map = L.map('map').setView([51.505, -0.09], 13);
+import axios from 'axios';
+import L from 'leaflet';
 
-L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=0FpdZwCBDclducjXH2WE', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-}).addTo(Map);
+const loadMap = () => {
+    axios.get('/demo/api/vehicle_data/YV2AS02A76B424444').then(response => {
+        return response.data.map(coordinatesDataEntry => {
+            return [
+                coordinatesDataEntry.latitude,
+                coordinatesDataEntry.longitude,
+            ]
+        });
+    }).then((path) => {
+        const center = path[path.length - 1];
+        const Map = L.map('map').setView(center, 13);
 
-L.marker([51.505, -0.09]).addTo(Map);
+        L.marker(center).addTo(Map);
+        L.polyline(path).addTo(Map);
+
+        L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=0FpdZwCBDclducjXH2WE', {
+            attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+        }).addTo(Map);
+    });
+};
+
+window.onload = () => {
+    loadMap();
+};
