0b21366b5c6e167abfe8bf3ab06a90ac9b75a195
jonagade <jonas.gadeikis9@gmail.com>
2019-12-08 21:50:11 +0200

    #3 Make Map Move With The Vehicle Every .2 s

--- assets/js/components/Pages/Vehicle/map.js
+++ assets/js/components/Pages/Vehicle/map.js
@@ -10,14 +10,26 @@ const loadMap = () => {
             ]
         });
     }).then((path) => {
-        const center = path[path.length - 1];
-        const Map = L.map('map').setView(center, 13);
+        let currentPath = [];
 
-        L.marker(center).addTo(Map);
-        L.polyline(path).addTo(Map);
+        currentPath.push(path[0]);
+        path.shift();
+
+            setInterval(() => {
+                currentPath.push(path.shift());
+
+                L.polyline(currentPath).addTo(Map);
+                Map.panTo(currentPath[currentPath.length - 1]);
+
+                Marker.setLatLng(currentPath[currentPath.length - 1]);
+            }, 200);
+
+        const Map = L.map('map').setView(currentPath[currentPath.length - 1], 13);
         L.tileLayer('https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}.png?key=0FpdZwCBDclducjXH2WE', {
             attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
         }).addTo(Map);
+
+        const Marker = L.marker(currentPath[currentPath.length - 1]).addTo(Map);
     });
 };
 
