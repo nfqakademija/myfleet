c00b1e0de8ba5faf26e5417d45bc3ca56e974a5a
jonagade <jonas.gadeikis9@gmail.com>
2019-12-09 17:10:09 +0200

    #3 Comment Instant Notification Code

--- assets/js/components/instantNotifications.js
+++ assets/js/components/instantNotifications.js
@@ -1,30 +1,30 @@
-import $ from 'jquery';
-
-function getContent(timestamp) {
-    const queryString = {'timestamp' : timestamp};
-
-    $.ajax(
-        {
-            type: 'GET',
-            url: '/api/getInstantNotification',
-            data: queryString,
-            success: function(data){
-                // put result data into "obj"
-                const obj = jQuery.parseJSON(data);
-                // put the data_from_file into #response
-                console.log(obj);
-                // call the function again, this time with the timestamp we just got from server.php
-                getContent(obj.timestamp);
-            }
-        }
-    );
-}
-
-// initialize jQuery
-$(function() {
-    getContent();
-});
-
-window.onload = () => {
-    getContent();
-};
+// import $ from 'jquery';
+//
+// function getContent(timestamp) {
+//     const queryString = {'timestamp' : timestamp};
+//
+//     $.ajax(
+//         {
+//             type: 'GET',
+//             url: '/api/getInstantNotification',
+//             data: queryString,
+//             success: function(data){
+//                 // put result data into "obj"
+//                 const obj = jQuery.parseJSON(data);
+//                 // put the data_from_file into #response
+//                 console.log(obj);
+//                 // call the function again, this time with the timestamp we just got from server.php
+//                 getContent(obj.timestamp);
+//             }
+//         }
+//     );
+// }
+//
+// // initialize jQuery
+// $(function() {
+//     getContent();
+// });
+//
+// window.onload = () => {
+//     getContent();
+// };
