7f0ef796383850775556722f6e36097a6c69d4a2
Artūras Šeršniovas <arturaz@gmail.com>
2019-12-10 22:12:47 +0200

    Add ROLE_ADMIN and ROLE_MANAGER

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -19,11 +19,29 @@ class UserFixtures extends Fixture
     {
         $user = new User();
         $user->setEmail('administratorius@imone.lt');
+        $user->setRoles(['ROLE_ADMIN']);
         $user->setPassword($this->passwordEncoder->encodePassword(
             $user,
             'password'
         ));
+        $manager->persist($user);
 
+        $user = new User();
+        $user->setEmail('administratorius@imone.ltu');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'passwordu'
+        ));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('vadybininkas@imone.lt');
+        $user->setRoles(['ROLE_MANAGER']);
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'password'
+        ));
         $manager->persist($user);
 
         $manager->flush();
