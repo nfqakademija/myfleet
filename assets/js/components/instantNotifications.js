a87117f76164e36f2dbeeba7a968ab92c1ac4dfe
jonagade <jonas.gadeikis9@gmail.com>
2019-12-11 18:09:30 +0200

    #3 Add Instant Notification Method

--- assets/js/components/instantNotifications.js
+++ assets/js/components/instantNotifications.js
@@ -1,23 +1,57 @@
-// import $ from 'jquery';
-//
-// function getContent() {
-//     $.ajax(
-//         {
-//             type: 'GET',
-//             url: '/api/instant_notification',
-//             success: function(){
-//                 console.log();
-//                 getContent();
-//             }
-//         }
-//     );
-// }
-//
-// // initialize jQuery
-// $(function() {
-//     getContent();
-// });
-//
-// window.onload = () => {
-//     getContent();
-// };
+import $ from 'jquery';
+import axios from 'axios';
+
+function getContent() {
+    $.ajax(
+        {
+            type: 'GET',
+            url: '/api/instant_notification',
+            success: function(){
+                if(data.description === undefined) {
+
+                    getContent();
+                    return;
+                }
+
+                let snackbar = $('.snackbar-template');
+
+                snackbar.clone();
+                snackbar.removeClass('snackbar-template');
+                snackbar.find('span').html(data.description);
+                snackbar.find('i').click(function () {
+                    $(this).parents('.Snackbar-value').remove();
+                });
+                snackbar.show();
+                snackbar.parent().append(snackbar);
+
+                getContent();
+            }
+        }
+    );
+
+    // axios.get('/api/instant_notification').then(response => {
+    //     if(response.data.description === undefined) {
+    //
+    //         getContent();
+    //         return;
+    //     }
+    //
+    //     let snackbar = $('.snackbar-template');
+    //
+    //     snackbar.clone();
+    //     snackbar.removeClass('snackbar-template');
+    //     snackbar.find('span').html(response.data.description);
+    //     snackbar.find('i').click(function () {
+    //         $(this).parents('.Snackbar-value').remove();
+    //     });
+    //     snackbar.show();
+    //     snackbar.parent().append(snackbar);
+    //
+    //     getContent();
+    // })
+}
+
+// initialize jQuery
+$(function() {
+    getContent();
+});
