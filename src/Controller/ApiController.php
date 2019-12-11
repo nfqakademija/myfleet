69764d677b53501d9257bada7ad06a72ee439639
Artūras Šeršniovas <arturaz@gmail.com>
2019-12-11 22:07:56 +0200

    Add Endpoint POST /api/emergency_call/{vin}

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,15 +2,13 @@
 
 namespace App\Controller;
 
-use App\Repository\InstantNotificationRepository;
 use App\Service\Action\ApiGetInstantNotificationAction;
 use App\Service\Action\ApiGetVehicleCoordinatesAction;
+use App\Service\Action\ApiPostVehicleEmergencyCallAction;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 
 class ApiController extends AbstractController
 {
@@ -18,6 +16,7 @@ class ApiController extends AbstractController
      * @Route("/api/instant_notification", name="api_instant_notificaiton")
      *
      * @param ApiGetInstantNotificationAction $action
+     *
      * @return Response
      */
     public function getInstantNotification(ApiGetInstantNotificationAction $action)
@@ -34,6 +33,19 @@ class ApiController extends AbstractController
      * @return mixed
      */
     public function getVehicleCoordinates(Request $request, ApiGetVehicleCoordinatesAction $action)
+    {
+        return $action->execute($request);
+    }
+
+    /**
+     * @Route("/api/emergency_call/{vin}", methods={"POST"}, name="api_vehicle_emergency_call")
+     *
+     * @param Request $request
+     * @param ApiPostVehicleEmergencyCallAction $action
+     *
+     * @return mixed
+     */
+    public function postVehicleEmergencyCall(Request $request, ApiPostVehicleEmergencyCallAction $action)
     {
         return $action->execute($request);
     }
---
+++ src/Service/Action/ApiPostVehicleEmergencyCallAction.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Service\Action;
+
+use App\Entity\Event;
+use App\Entity\InstantNotification;
+use App\Entity\User;
+use App\Entity\Vehicle;
+use App\Repository\UserRepository;
+use App\Repository\VehicleRepository;
+use DateTime;
+use Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+
+class ApiPostVehicleEmergencyCallAction
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var VehicleRepository
+     */
+    private $vehicleRepository;
+
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param RouterInterface $router
+     * @param VehicleRepository $vehicleRepository
+     * @param UserRepository $userRepository
+     */
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        VehicleRepository $vehicleRepository,
+        UserRepository $userRepository,
+        RouterInterface $router
+    ) {
+        $this->entityManager = $entityManager;
+        $this->vehicleRepository = $vehicleRepository;
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+
+    public function execute(Request $request): Response
+    {
+        $vehicle = $this->getVehicle($request);
+        if (is_null($vehicle)) {
+            return new JsonResponse([
+                'status' => 'error',
+                'errorCode' => 404,
+                'errorMessage' => 'Vehicle Not Found!'
+            ]);
+        }
+
+        $this->addEventToVehicle($vehicle);
+        $this->addNotificationToUsers($vehicle);
+
+        return new JsonResponse(['status' => 'success']);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Vehicle|null
+     */
+    private function getVehicle(Request $request): ?Vehicle
+    {
+        $vin = $request->attributes->get('vin');
+
+        return $this->vehicleRepository->findOneBy(['vin' => $vin]);
+    }
+
+    /**
+     * @param Vehicle $vehicle
+     *
+     * @throws Exception
+     */
+    private function addEventToVehicle(Vehicle $vehicle)
+    {
+        $event = new Event();
+        $event->setVehicle($vehicle);
+        $event->setCreatedAt(new DateTime());
+        $event->setDescription('Paspaustas SOS mygtukas!');
+
+        $this->entityManager->persist($event);
+        $this->entityManager->flush();
+    }
+
+    private function addNotificationToUsers(Vehicle $vehicle)
+    {
+        foreach ($this->userRepository->findByRole('ADMIN') as $user) {
+            $this->addNotificationToUser($vehicle, $user);
+        }
+
+        foreach ($vehicle->getUsers() as $user) {
+            $this->addNotificationToUser($vehicle, $user);
+        }
+    }
+
+    private function addNotificationToUser(Vehicle $vehicle, User $user)
+    {
+        $linkToVehicle = $this->router->generate('vehicle_view', [
+            'id' => $vehicle->getId(),
+        ]);
+
+        $description = 'Gautas SOS signalas: '
+            . '<a href="' . $linkToVehicle . '">' . $vehicle->getPlateNumber() . '</a>';
+
+        $instantNotification = new InstantNotification();
+        $instantNotification->setUser($user);
+        $instantNotification->setEventTime(new DateTime());
+        $instantNotification->setIsSent(false);
+        $instantNotification->setDescription($description);
+
+        $this->entityManager->persist($instantNotification);
+        $this->entityManager->flush();
+    }
+}
