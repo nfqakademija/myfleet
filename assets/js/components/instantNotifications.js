08fa044049ab5c32e0ce6c4e8e434e93c9bd007b
jonagade <jonas.gadeikis9@gmail.com>
2019-12-14 12:10:42 +0200

    #3 Show Modal if Form is not Valid

--- assets/js/components/instantNotifications.js
+++ assets/js/components/instantNotifications.js
@@ -2,31 +2,31 @@ import $ from 'jquery';
 import axios from 'axios';
 
 function getContent() {
-    $.ajax(
-        {
-            type: 'GET',
-            url: '/api/instant_notification',
-            success: function(data){
-                if(data.description === undefined) {
-
-                    getContent();
-                    return;
-                }
-
-                let snackbar = $('.snackbar-template').clone();
-
-                snackbar.removeClass('snackbar-template');
-                snackbar.find('span').html(data.description);
-                snackbar.find('i').click(function () {
-                    $(this).parents('.Snackbar-value').remove();
-                });
-                $('.snackbar-template').parent().append(snackbar);
-                snackbar.show();
-
-                getContent();
-            }
-        }
-    );
+    // $.ajax(
+    //     {
+    //         type: 'GET',
+    //         url: '/api/instant_notification',
+    //         success: function(data){
+    //             if(data.description === undefined) {
+    //
+    //                 getContent();
+    //                 return;
+    //             }
+    //
+    //             let snackbar = $('.snackbar-template').clone();
+    //
+    //             snackbar.removeClass('snackbar-template');
+    //             snackbar.find('span').html(data.description);
+    //             snackbar.find('i').click(function () {
+    //                 $(this).parents('.Snackbar-value').remove();
+    //             });
+    //             $('.snackbar-template').parent().append(snackbar);
+    //             snackbar.show();
+    //
+    //             getContent();
+    //         }
+    //     }
+    // );
 
     // axios.get('/api/instant_notification').then(response => {
     //     if(response.data.description === undefined) {
@@ -53,6 +53,6 @@ function getContent() {
 }
 
 // initialize jQuery
-$(function() {
-    getContent();
-});
+// $(function() {
+//     getContent();
+// });
