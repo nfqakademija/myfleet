b6f38c0d689a7a4d69504c141c92404d415b1653
jonagade <jonas.gadeikis9@gmail.com>
2019-12-17 13:09:41 +0200

    #3 Remove commented code from instantNotification.js

--- assets/js/components/instantNotifications.js
+++ assets/js/components/instantNotifications.js
@@ -27,29 +27,6 @@ function getContent() {
             }
         }
     );
-
-    // axios.get('/api/instant_notification').then(response => {
-    //     if(response.data.description === undefined) {
-    //
-    //         getContent();
-    //         return;
-    //     }
-    //
-    //     let snackbar = $('.snackbar-template');
-    //
-    //     snackbar.clone();
-    //     snackbar.removeClass('snackbar-template');
-    //     snackbar.find('span').html(response.data.description);
-    //     snackbar.find('i').click(function () {
-    //         $(this).parents('.Snackbar-value').remove();
-    //     });
-    //     snackbar.show();
-    //     snackbar.parent().append(snackbar);
-    //
-    //     getContent();
-    // }).catch(() => {
-    //     getContent();
-    // });
 }
 
 $(function() {
